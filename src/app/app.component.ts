08c9e6e78c2826a6eea1d7f8db0f2dbce892ffa3
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-25 19:10:06 -0300

    Primeiras alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'first-class';
+  description = 'Todo Poderoso';
+  url = 'https://i.pinimg.com/564x/4b/0a/1f/4b0a1f1cfbba523a8e6c6ee8341a6520.jpg';
+
 }
