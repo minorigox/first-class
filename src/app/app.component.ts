6184e5f50b9b08f62455fd7092010717d48ea968
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-26 11:30:56 -0300

    Configuração do módulo CommonModule.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PhotoComponent } from './photo/photo.component';
+import { PhotoComponent } from './photos/photo/photo.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PhotoComponent],
+  imports: [RouterOutlet, PhotoComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-class';
+  photos = [
+    {      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeTm8ZtFAliBMdrbKpVcRlOx7YPSKGCEakIQ&s',
+      description: 'Todo Poderoso'
+    },
+    {      
+url: 'https://res.cloudinary.com/idemo/image/fetch/c_fill,f_webp,q_auto:good/https://www.jornaldorap.com.br/wp-content/uploads/2024/09/depay.jpg',
+      description: 'Memphis'
+    }  
+   ];
 }
