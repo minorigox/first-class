16d826661771e9d5d2bce62d05f099008aaf4411
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-28 17:23:33 -0300

    Ajustes para funcionamento do httpClient.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PhotoComponent } from './photos/photo/photo.component';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PhotoComponent, CommonModule],
+  imports: [PhotoComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-class';
-  photos = [
-    {      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeTm8ZtFAliBMdrbKpVcRlOx7YPSKGCEakIQ&s',
-      description: 'Todo Poderoso'
-    },
-    {      
-url: 'https://res.cloudinary.com/idemo/image/fetch/c_fill,f_webp,q_auto:good/https://www.jornaldorap.com.br/wp-content/uploads/2024/09/depay.jpg',
-      description: 'Memphis'
-    }  
-   ];
+  photos: Object[] = [];
+  constructor(http: HttpClient) {
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe((photos) => (this.photos = photos));
+  }
 }
